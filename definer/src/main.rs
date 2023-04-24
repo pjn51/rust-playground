ca475c67fe627d0dfddcb6d52c60781f4f5c0410
pjn51 <58340957+pjn51@users.noreply.github.com>
2023-04-23 17:36:04 -0700

    working on definer

--- definer/src/main.rs
+++ definer/src/main.rs
@@ -1,19 +1,44 @@
 // imports
 use dotenv::dotenv;
 use std::env;
+use reqwest::blocking::Client;
 
-fn api_call(api_url:&str, api_key:String) {
-    let api_url = api_url.to_owned() + &api_key;
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let response = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
+//         .await?
+//         .text()
+//         .await?;
+//     println!("{}", response);
+//     Ok(())
+// }
 
-    
+
+async fn caller(api_key:String, word:String) -> Result<(), E> {
+    // url construction
+    let mut site_url = String::from("https://www.dictionaryapi.com/api/v3/references/collegiate/json/word_val?key=key_val");
+    site_url = site_url.replace("word_val", &word);
+    site_url = site_url.replace("key_val", &api_key);
+    println!("Getting definition for '{}'", word);
+
+    // api call
+    let response = Client::new()
+        .get(&site_url)
+        .send()?
+        .text()?;
+
+    println!("\n{}\n", response);
+
+    Ok(())
 }
 
 fn main() {
-    let dict_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=";
     dotenv().ok();
     let api_key = env::var("API_KEY").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let word = &args[1];
 
-    api_call(dict_url, api_key);
+    let result = caller(api_key, word.to_string());
 
     //dict_url.push_str(api_key);
    
